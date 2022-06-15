2002f7d6ee9c0a34a8aaab2301852a3d13a333c6
destello501 <107224817+destello501@users.noreply.github.com>
2022-06-15 09:32:01 -0500

    Add files via upload

---
+++ nose/a/main.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+
+       duration = 150
+
+    });
+
+
+
+});
